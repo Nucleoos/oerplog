b5d7d1f12ff640e4e910ace321471e52a4511e40
Yanina Aular (Vauxoo) <yanina.aular@vauxoo.com>
2014-06-29 10:16:05 -0430

    [IMP] code package field is added, setup.py is configured

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='OERPLog',
+      version='1.0',
+      description='Display Log of OpenERP in ',
+      author='Yanina Aular',
+      author_email='yaniaular@gmail.com',
+      url='http://www.yanina.com.ve',
+      py_modules=['oerplog'],
+      #packages=['distutils', 'distutils.command'],
+     )
