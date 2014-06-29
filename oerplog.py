8443d473138e4201ffbd3e5685682f48d409b350
Yanina Aular (Vauxoo) <yanina.aular@vauxoo.com>
2014-06-28 21:59:38 -0430

    [IMP] improvements in script

---
+++ oerplog.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import argparse
+import re
+import pdb
+import os
+
+def arguments():
+    parser = argparse.ArgumentParser() 
+    parser.add_argument("-lf", "--logfile", help="Path log file", required=True)
+    parser.add_argument("-o", "--outfile", help="Path HTML out", default='.')
+    args = parser.parse_args()
+    if( args.logfile is None):
+        print "Must be specified a path with log file"
+        quit()
+    infile = args.logfile
+    outfile = args.outfile
+    return infile, outfile
+
+def open_logfile(infile):
+
+    with open(infile, 'r') as f:
+        logfile_str = f.read()
+        f.close()
+    return logfile_str
+
+def logstr_to_loghtml(logfile_str):
+
+    logfile_html = logfile_str
+
+    search_info = re.compile(r" INFO ")
+    logfile_html = re.sub(search_info," <b class='info'>INFO</b> ",logfile_html)
+
+    search_warning = re.compile(r" WARNING ")
+    logfile_html = re.sub(search_warning," <b class='warning'>WARNING</b> ",logfile_html)
+
+    search_error = re.compile(r" ERROR ")
+    logfile_html = re.sub(search_error," <b class='error'>ERROR</b> ",logfile_html)
+
+    search_test = re.compile(r" TEST ")
+    logfile_html = re.sub(search_test," <b class='test'>TEST</b> ",logfile_html)
+
+    search_critical = re.compile(r" CRITICAL ")
+    logfile_html = re.sub(search_critical," <b class='critical'>CRITICAL</b> ",logfile_html)
+
+    search_debug = re.compile(r" DEBUG ")
+    logfile_debug = re.sub(search_debug," <b class='debug'>DEBUG</b> ",logfile_html)
+
+    return logfile_html
+
+def get_html_header():
+
+    return '<html> <head> <link rel="stylesheet" media="all" href="openerp_log.css"> </head>\
+            <body> <pre>'
+
+def get_html_footer():
+
+    return '</pre> </body> </html>'
+
+def save_loghtml(logfile_html, outfile):
+    outfile += 'index.html'
+
+    head = get_html_header()
+    foot = get_html_footer()
+
+    with open(outfile, 'w') as f:
+        f.write(head + logfile_html + foot)
+        f.close()
+
+def get_openerp_log_css(outfile):
+    os.system('cp openerp_log.css %s' % outfile)
+
+if __name__ == '__main__':
+    infile, outfile = arguments()
+    logfile_str = open_logfile(infile)
+    logfile_html = logstr_to_loghtml(logfile_str)
+    res = save_loghtml(logfile_html, outfile)
+    get_openerp_log_css(outfile)
