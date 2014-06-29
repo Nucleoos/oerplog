525fde6057408880c5fe5b9cd49f7dbadc831b32
Yanina Aular (Vauxoo) <yanina.aular@vauxoo.com>
2014-06-29 10:37:32 -0430

    [ADD] bin field, run method added, call to run in setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,10 @@ setup(name='OERPLog',
       author_email='yaniaular@gmail.com',
       url='http://www.yanina.com.ve',
       packages=['oerplog'],
+      scripts=['bin/oerplog'],
+#~      entry_points="""
+#~      [console_scripts]
+#~      oerplog=oerplog.oerplog:run
+#~      """,
       #py_modules=['oerplog'],
      )
