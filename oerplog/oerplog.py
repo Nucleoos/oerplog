0969f018e0198453e794d829cb855f9087de0090
Yanina Aular (Vauxoo) <yanina.aular@vauxoo.com>
2014-06-29 21:05:01 -0430

    [IMP] improvements in manager of server

--- oerplog/oerplog.py
+++ oerplog/oerplog.py
@@ -15,11 +15,9 @@ def arguments():
     parser = argparse.ArgumentParser() 
     parser.add_argument("-lf", "--logfile", help="Path log file", required=True)
     parser.add_argument("-o", "--outfile", help="Path HTML out", default='./')
-    parser.add_argument("-r", "--runserver", help="Run Server Localhost")
+    parser.add_argument("-r", "--runserver", help="Run Server Localhost", default=False)
     parser.add_argument("-p", "--port", help="Server Port", default='4444')
     args = parser.parse_args()
-
-    #runserver = False
 
     if( args.logfile is None):
         print "Must be specified a path with log file"
@@ -34,7 +32,7 @@ def arguments():
     infile = args.logfile
     outfile = args.outfile
     port = args.port
-    runserver = True
+    runserver = args.runserver
     return infile, outfile, runserver, port
 
 def open_logfile(infile):
@@ -69,26 +67,28 @@ def logstr_to_loghtml(logfile_str):
 
     return logfile_html
 
-def get_html_header():
+def get_html_header(runserver):
 
-    return '<html> <head> <link rel="stylesheet" media="all" \
-href="/usr/local/lib/python2.7/dist-packages/oerplog/openerp_log.css"> <META HTTP-EQUIV="REFRESH" \
-CONTENT="1;URL=log.html"></head> \
-<body> <pre>'
-#~    return '<html> <head> <link rel="stylesheet" media="screen" \
-#~href="openerp_log.css"><meta http-equiv="content-Type" content="text/html; charset=utf-8" /> <META HTTP-EQUIV="REFRESH" CONTENT="5;URL=log.html"></head> \
-#~<body> <pre>'
+    if runserver: 
+        return '<html> <head> <link rel="stylesheet" media="screen" \
+        href="openerp_log.css"><meta http-equiv="content-Type" content="text/html; charset=utf-8" /> <META HTTP-EQUIV="REFRESH" CONTENT="5;URL=log.html"></head> \
+        <body> <pre>'
+    else:
+        return '<html> <head> <link rel="stylesheet" media="all" \
+        href="/usr/local/lib/python2.7/dist-packages/oerplog/openerp_log.css"> <META HTTP-EQUIV="REFRESH" \
+        CONTENT="1;URL=log.html"></head> \
+        <body> <pre>'
 
 
 def get_html_footer():
     return '</pre> </body> </html>'
 
-def save_loghtml(logfile_html, outfile):
+def save_loghtml(logfile_html, outfile, runserver):
     dircurrent = commands.getoutput('pwd')
     outfile = dircurrent + '/' + outfile
     outfile += 'log.html'
 
-    head = get_html_header()
+    head = get_html_header(runserver)
     foot = get_html_footer()
     with open(outfile, 'w') as f:
         f.write(head + logfile_html + foot)
@@ -100,32 +100,35 @@ def get_openerp_log_css(outfile):
     os.system('cp /usr/local/lib/python2.7/dist-packages/oerplog/openerp_log.css %s' % outfile)
     pass
 
-def main(infile, outfile, num):
+def main(infile, outfile, runserver, num):
+    if num != 0:
+        time.sleep(3)
     logfile_str = open_logfile(infile)
     logfile_html = logstr_to_loghtml(logfile_str)
-    time.sleep(5)
-    res = save_loghtml(logfile_html, outfile)
-    if num == 0:
+    res = save_loghtml(logfile_html, outfile, runserver)
+    if num == 0 and runserver:
         get_openerp_log_css(outfile)
     return num + 1
 
-def run_server():
-    port = '4444'
-    os.system('xdg-open http://localhost:%s/log.html && python -m SimpleHTTPServer %s' % (port,port))
+def run_server(port):
+    os.system('python -m SimpleHTTPServer %s' % (port))
 
 def run():
 
     infile, outfile, runserver, port = arguments()
+    w = threading.Thread(target=run_server, name='Server', args=(port, ) )
     num = 0
     print "Loading log.html..."
     while(True):
-        num = main(infile, outfile, num)
+        num = main(infile, outfile, runserver, num)
         if num == 1:
-            time.sleep(1)
-            logging.info('Running log.html...')
-            #w = threading.Thread(target=run_server, name='Server')
-            #w.start()
-            os.system('xdg-open log.html &')
+            if runserver:
+                logging.info('Running Server locahost:%s/log.html...' % (port))
+                w.start()
+                os.system('xdg-open http://localhost:%s/log.html' % (port))
+            else:
+                logging.info('Running log.html...')
+                os.system('xdg-open log.html &')
 
 if __name__ == '__main__':
     run()
